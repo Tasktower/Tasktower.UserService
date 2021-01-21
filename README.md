b204a52a4f010f546c59e70d40e5c642d1e04e78
obmeir <orenbm11@gmail.com>
2021-01-21 02:55:29 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+﻿# Tasktower.UserService
+This is a redesign of the tasktower-auth service which was originally built using Go.
+This version instead uses the .NET framework and it's tooking.
---
+++ test/Tasktower.UserService.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Tasktower.UserService.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
