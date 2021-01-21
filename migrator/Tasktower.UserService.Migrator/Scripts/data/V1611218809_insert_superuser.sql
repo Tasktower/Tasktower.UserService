ec6763a99bdefe512de33fadd15d1272b693fd33
obmeir <orenbm11@gmail.com>
2021-01-21 05:44:18 -0500

    miner audit

--- migrator/Tasktower.UserService.Migrator/Scripts/data/V1611218809_insert_superuser.sql
+++ migrator/Tasktower.UserService.Migrator/Scripts/data/V1611218809_insert_superuser.sql
@@ -2,7 +2,7 @@
 
 -- Insert default super user
 
--- Super user ID is always 1
+-- Super user ID is always 1 for development purposes, Todo: add hashed password.
 INSERT INTO users(id, name, email, email_verified, roles)
 VALUES('00000000-0000-0000-0000-000000000001', 'admin', 'dummyadmin@example.com', TRUE, '{SUPERUSER}');
 
